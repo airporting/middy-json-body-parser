1eb4ad3d9582ff1eb889214cbbe58944a0ee7b74
Mathieu KIM ROBIN <mthrobin@gmail.com>
2023-12-04 18:44:18 +0100

    feat(version): bump

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const httpJsonBodyParserMiddleware = () => {
   async function before(request) {
-    const { body } = request.event;
-
+    console.log(request);
     try {
+      const { body } = request.event;
+
+      console.log('will parse body');
+      console.log('body', body);
       const data = request.event.isBase64Encoded
         ? Buffer.from(body, 'base64').toString()
         : body;
 
       request.event.body = JSON.parse(data);
+      console.log({ got: request.event.body });
     } catch (err) {
       console.warn('Invalid or malformed JSON was provided');
     }
